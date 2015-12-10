8781020791df74e041a4be307b87b548a8553029
Jason Garber <jason@sixtwothree.org>
2015-12-09 19:31:00 -0500

    Add Capistrano and related config.

--- Gemfile
+++ Gemfile
@@ -3,8 +3,10 @@ ruby '2.2.3'
 source 'https://rubygems.org' do
   gem 'sinatra', '~> 1.4'
   gem 'mini_magick', '~> 4.3'
+  gem 'puma', '~> 2.15'
 
   group :development do
+    gem 'capistrano', '~> 2.15'
     gem 'shotgun', '~> 0.9.1'
     gem 'tux', '~> 0.3.0'
   end
---
+++ config/deploy.rb
@@ -0,0 +1,19 @@
+set :stages, %w(production)
+set :default_stage, 'production'
+
+require 'bundler/capistrano'
+require 'capistrano/ext/multistage'
+
+set :application, 'spaceholder.cc'
+
+set :repository, 'git@github.com:jgarber623/spaceholder.cc.git'
+set :deploy_to, '/var/www/spaceholder-cc'
+set :user, 'www-data'
+set :deploy_via, :remote_cache
+set :ssh_options, { forward_agent: true }
+set :use_sudo, false
+
+set :ruby_version, '2.2.3'
+set :bundle_cmd, %{chruby-exec #{ruby_version} -- bundle}
+
+default_run_options[:shell] = '/bin/bash'
