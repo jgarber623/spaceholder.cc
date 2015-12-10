e0aedfe71e231a5ee3b3fe6c6e8086b464394487
Jason Garber <jason@sixtwothree.org>
2015-12-09 20:36:42 -0500

    Update deploy tasks.

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,5 +15,20 @@ set :use_sudo, false
 
 set :ruby_version, '2.2.3'
 set :bundle_cmd, "chruby-exec #{ruby_version} -- bundle"
+set :pumactl_cmd, "#{bundle_cmd} exec pumactl -F config/puma.rb"
 
 default_run_options[:shell] = '/bin/bash'
+
+namespace :deploy do
+  task :restart do
+    run "cd #{current_path} && #{pumactl_cmd} restart"
+  end
+
+  task :start do
+    run "cd #{current_path} && #{pumactl_cmd} start"
+  end
+
+  task :stop do
+    run "cd #{current_path} && #{pumactl_cmd} stop"
+  end
+end
