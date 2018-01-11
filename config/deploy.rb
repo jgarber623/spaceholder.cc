83a562a7501de3ec9b14ed7f867eaab56a6599e7
Jason Garber <jason@sixtwothree.org>
2018-01-10 21:20:57 -0500

    Update gems

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.10.0'
+lock '3.10.1'
 
 set :application, 'spaceholder.cc'
 set :repo_url, 'git@github.com:jgarber623/spaceholder.cc.git'
