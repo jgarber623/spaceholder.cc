5a6e76a3b1eed4cc02b0fdca5441194e94f00404
Jason Garber <jason@sixtwothree.org>
2018-05-22 11:38:24 -0400

    Upgrade app to Ruby 2.5.1

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.10.2'
 
 set :application, 'spaceholder.cc'
 set :repo_url, 'git@github.com:jgarber623/spaceholder.cc.git'
-set :chruby_ruby, 'ruby-2.4.2'
+set :chruby_ruby, 'ruby-2.5.1'
 
 append :linked_dirs, 'tmp/pids', 'tmp/sockets'
 
