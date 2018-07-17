242ae2bf834160efca11b9fcf89d1c5927af7ad2
Jason Garber <jason@sixtwothree.org>
2018-07-17 08:36:53 -0400

    Reorganize specs as request-ish specs

--- spec/lib/spaceholder_spec.rb
+++
@@ -1,5 +0,0 @@
-describe Spaceholder do
-  it 'is a module' do
-    expect(described_class).to be_a(Module)
-  end
-end
---
+++ spec/requests/error_page_spec.rb
@@ -0,0 +1,17 @@
+describe Spaceholder::App do
+  let(:error_page_content) { '“Houston, we’ve had <a href="http://apollo13.spacelog.org/02:07:55:35/#log-line-201335">a problem</a>.”' }
+
+  context 'when GET /foo' do
+    before do
+      get '/foo'
+    end
+
+    it 'returns a not found HTTP status code' do
+      expect(last_response.status).to eq(404)
+    end
+
+    it 'renders the 404 view' do
+      expect(last_response.body).to include(error_page_content)
+    end
+  end
+end
