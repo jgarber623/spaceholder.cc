d8a28db50d283224b56291f0d6bb5e104dd06e31
Jason Garber <jason@sixtwothree.org>
2019-05-11 21:41:20 -0400

    Add run.app URL to HostRedirect

--- lib/spaceholder/app.rb
+++ lib/spaceholder/app.rb
@@ -16,7 +16,7 @@ module Spaceholder
 
     configure :production do
       use Rack::SslEnforcer, redirect_html: false
-      use Rack::HostRedirect, %w[www.spaceholder.cc] => 'spaceholder.cc'
+      use Rack::HostRedirect, %w[spaceholder-cc-web-6qapjeg6uq-uc.a.run.app www.spaceholder.cc] => 'spaceholder.cc'
       use Rack::Deflater
     end
 
