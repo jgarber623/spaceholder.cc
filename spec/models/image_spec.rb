2bf81ab5acde1aabb7f3d327d37f58ac23af7ff2
Jason Garber <jason@sixtwothree.org>
2023-01-02 21:39:51 -0500

    Remove most Ruby-centric code

--- spec/models/image_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe Image do
-  subject(:image) { described_class.new(200, 100) }
-
-  describe '#process' do
-    it { expect(image.process).to be_a(Tempfile) }
-  end
-end
--- spec/support/shared_contexts/roda_app.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.shared_context 'when Roda app', roda: :app do
-  subject(:response) { last_response }
-end
