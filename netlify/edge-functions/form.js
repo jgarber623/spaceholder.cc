b92d2b02cf09b91e9e9b26f93755c799c3d53146
Jason Garber <jason@sixtwothree.org>
2023-01-04 11:15:00 -0500

    Refactor homepage form Edge Function

--- netlify/edge-functions/form.js
+++ netlify/edge-functions/form.js
@@ -1,24 +1,23 @@
 export default async (request, context) => {
   let url = new URL(request.url);
 
-  if (request.method === 'POST' && request.headers.get('content-type') === 'application/x-www-form-urlencoded') {
-    let body = await request.clone().formData();
-    let params = Object.fromEntries(body)
+  if (url.pathname === '/i' && request.method === 'POST') {
+    let regexp = new RegExp(/^application\/x-www-form-urlencoded(?:;.+)?$/);
 
-    if (params.width && params.height) {
-      return new Response(null, {
-        status: 302,
-        headers: {
-          location: `/i/${params.width}/${params.height}`
-        }
-      });
+    if (regexp.test(request.headers.get('content-type'))) {
+      let body = await request.clone().formData();
+      let params = Object.fromEntries(body);
+
+      if (params.width && params.height) {
+        return new Response(null, {
+          status: 302,
+          headers: {
+            location: `/i/${params.width}/${params.height}`
+          }
+        });
+      }
     }
   }
 
-  return new Response(null, {
-    status: 301,
-    headers: {
-      location: '/'
-    }
-  });
+  return context.next();
 };
