4ae59bed4eec8dcae41f9efe8ac29099301289a6
Jason Garber <jason@sixtwothree.org>
2023-07-09 23:08:39 -0400

    Address ESLint errors

--- netlify/edge-functions/form.js
+++ netlify/edge-functions/form.js
@@ -1,15 +1,15 @@
 export default async (request, context) => {
-  let requestId = request.headers.get('x-nf-request-id');
-  let url = new URL(request.url);
+  const requestId = request.headers.get('x-nf-request-id');
+  const url = new URL(request.url);
 
   console.log(requestId, 'Received request:', request.method, request.url);
 
   if (url.pathname === '/i' && request.method === 'POST') {
-    let regexp = new RegExp(/^application\/x-www-form-urlencoded(?:;.+)?$/);
+    const regexp = /^application\/x-www-form-urlencoded(?:;.+)?$/;
 
     if (regexp.test(request.headers.get('content-type'))) {
-      let body = await request.clone().formData();
-      let params = Object.fromEntries(body);
+      const body = await request.clone().formData();
+      const params = Object.fromEntries(body);
 
       if (params.width && params.height) {
         console.log(requestId, 'Generating response for request with params:', params);
