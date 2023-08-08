05fe6785d6851654eb0a33e8556309e400f9a38d
Jason Garber <jason@sixtwothree.org>
2023-08-07 22:41:44 -0400

    Streamline Eleventy config

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,17 +1,21 @@
 module.exports = function(eleventyConfig) {
+  // Global Data
+  eleventyConfig.addGlobalData('app', require('./src/manifest.webmanifest.json'));
+
   // Passthrough File Copy
   eleventyConfig
     .addPassthroughCopy('./src/*.{ico,txt}')
-    .addPassthroughCopy('./src/assets/images');
+    .addPassthroughCopy('./src/assets/images')
+    .addPassthroughCopy({
+      './src/manifest.webmanifest.json': 'manifest.webmanifest'
+    });
 
   // Plugins
   eleventyConfig.addPlugin(require('./lib/plugins/postcss.js'));
 
   return {
     dir: {
-      input: './src',
-      layouts: '_layouts',
-      output: './public'
+      input: './src'
     }
   };
 };
